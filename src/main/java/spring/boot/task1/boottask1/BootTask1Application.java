7eb6451591dd1a41636dac2dccf37f818ffab43b
uvuso <vuse24021995>
2020-06-24 12:10:46 +0300

    first commit

---
+++ src/main/java/spring/boot/task1/boottask1/BootTask1Application.java
@@ -0,0 +1,13 @@
+package spring.boot.task1.boottask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTask1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootTask1Application.class, args);
+    }
+
+}
---
+++ src/test/java/spring/boot/task1/boottask1/BootTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.task1.boottask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootTask1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
