3249f9d2ceb3e8e074c49cbedfda4716ae08bca1
uvuso <vuse24021995>
2020-06-26 13:25:27 +0300

    Added parser

---
+++ src/main/java/spring/boot/task1/boottask1/service/impl/CsvFileParserService.java
@@ -0,0 +1,39 @@
+package spring.boot.task1.boottask1.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import spring.boot.task1.boottask1.model.CsvGroupedDates;
+import spring.boot.task1.boottask1.service.FileParserService;
+import spring.boot.task1.boottask1.service.FileService;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CsvFileParserService implements FileParserService<List<CsvGroupedDates>> {
+    @Autowired
+    private FileService fileService;
+
+    @Override
+    public List<CsvGroupedDates> parseFile(String path) {
+        return fileService.readFile(path)
+                .stream()
+                .map(this::parseString)
+                .collect(Collectors.toList());
+    }
+
+    private CsvGroupedDates parseString(String line) {
+        CsvGroupedDates dates = new CsvGroupedDates();
+        String[] mass = line.split(",");
+        dates.setId(Long.parseLong(mass[0]));
+        dates.setProductId(mass[1]);
+        dates.setUserId(mass[2]);
+        dates.setProfileName(mass[3]);
+        dates.setHelpfulnessNumerator(Long.parseLong(mass[4]));
+        dates.setHelpfulnessDenominator(Long.parseLong(mass[5]));
+        dates.setScore(Long.parseLong(mass[6]));
+        dates.setTime(Long.parseLong(mass[7]));
+        dates.setSummary(mass[8]);
+        dates.setText(mass[9]);
+        return dates;
+    }
+}
